a9da63a587879e5204c4d1adb971fbbdcfb99e4b
Roldan Joaquin <jroldanarin@gmail.com>
2025-06-09 15:30:21 -0300

    refactor: sidebar active state and colors

--- trainup-web/src/navbar/Sidebar.jsx
+++ trainup-web/src/navbar/Sidebar.jsx
@@ -1,74 +1,115 @@
-import React, { useState } from 'react';
-import '../styles/sidebar.css';
-import { useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faHome, faCompass, faCheck, faUser, faSignOutAlt, faPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
-import { useLogin } from '../context/LoginContext';
+import React, { useState } from "react";
+import "../styles/sidebar.css";
+import { useLocation, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faHome,
+  faCompass,
+  faCheck,
+  faUser,
+  faSignOutAlt,
+  faPlus,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import { useLogin } from "../context/LoginContext";
 
 const Sidebar = () => {
-    const navigate = useNavigate();
-    const { user, restartLogin } = useLogin();
-    const [showSidebar, setShowSidebar] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { user, restartLogin } = useLogin();
+  const [showSidebar, setShowSidebar] = useState(false);
 
-    return (
-
-        <div className='nes'>
-        <button className='nes1' onClick={() => setShowSidebar(!showSidebar)}><FontAwesomeIcon icon={faBars} /></button>
-        <div className={`sidebar ${showSidebar ? `s-show` : `s-noshow`}`}>
-            
-            <ul>
-                    {user?.rol === "ADMIN" && (
-                    <li>
-                        <button className='sidebar-btn' onClick={() => navigate('/es/home/crear/rutina')}>
-                            <FontAwesomeIcon icon={faPlus} className="icon" /> <span>Nueva Rutina</span>
-                        </button>
-                    </li>
-                )}
-                <li>
-                    <button className='sidebar-btn' onClick={() => {
-                        navigate('/es/home')
-                        setShowSidebar(!showSidebar)
-                    }}>
-                        <FontAwesomeIcon icon={faHome} className="icon" /> <span>Home</span>
-                    </button>
-                </li>
-                <li>
-                    <button className='sidebar-btn' onClick={() => {
-                        setShowSidebar(!showSidebar)
-                        navigate('/es/home/explorador')}}>
-                        <FontAwesomeIcon icon={faCompass} className="icon" /> <span>Explorar</span>
-                    </button>
-                </li>
-                <li>
-                    <button className='sidebar-btn' onClick={() => {
-                        setShowSidebar(!showSidebar)
-                        navigate('/es/home/buscar')}}>
-                        <FontAwesomeIcon icon={faSearch} className="icon" /> <span>Buscar</span>
-                    </button>
-                </li>
-                <li>
-                    <button className='sidebar-btn' onClick={() => {
-                        setShowSidebar(!showSidebar)
-                        navigate('/es/home/completadas')}}>
-                        <FontAwesomeIcon icon={faCheck} className="icon" /> <span>Completadas</span>
-                    </button>
-                </li>
-                <li>
-                    <button className='sidebar-btn' onClick={() => {
-                        setShowSidebar(!showSidebar)
-                        navigate('/es/home/profile')}}>
-                        <FontAwesomeIcon icon={faUser} className="icon" /> <span>Perfil</span>
-                    </button>
-                </li>
-                <li className='sidebar-logout'>
-                    <button className='sidebar-btn' onClick={restartLogin}>
-                        <FontAwesomeIcon icon={faSignOutAlt} className="icon" /> <span>Cerrar sesion</span>
-                    </button>
-                </li>
-            </ul>
-        </div>
-        </div>
-    );
-}
+  return (
+    <div className="nes">
+      <button className="nes1" onClick={() => setShowSidebar(!showSidebar)}>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+      <div className={`sidebar ${showSidebar ? `s-show` : `s-noshow`}`}>
+        <ul>
+          {user?.rol === "ADMIN" && (
+            <li>
+              <button
+                className={`sidebar-btn ${
+                  location.pathname === "/es/home/crear/rutina" ? "active" : ""
+                }`}
+                onClick={() => navigate("/es/home/crear/rutina")}
+              >
+                <FontAwesomeIcon icon={faPlus} className="icon" />{" "}
+                <span>Nueva Rutina</span>
+              </button>
+            </li>
+          )}
+          <li>
+            <button
+              className={`sidebar-btn ${location.pathname === '/es/home' ? 'active' : ''}`}
+              onClick={() => {
+                navigate("/es/home");
+                setShowSidebar(!showSidebar);
+              }}
+            >
+              <FontAwesomeIcon icon={faHome} className="icon" />{" "}
+              <span>Inicio</span>
+            </button>
+          </li>
+          <li>
+            <button
+              className={`sidebar-btn ${location.pathname === '/es/home/explorador' ? 'active' : ''}`}
+              onClick={() => {
+                setShowSidebar(!showSidebar);
+                navigate("/es/home/explorador");
+              }}
+            >
+              <FontAwesomeIcon icon={faCompass} className="icon" />{" "}
+              <span>Explorar</span>
+            </button>
+          </li>
+          <li>
+            <button
+              className={`sidebar-btn ${location.pathname === '/es/home/buscar' ? 'active' : ''}`}
+              onClick={() => {
+                setShowSidebar(!showSidebar);
+                navigate("/es/home/buscar");
+              }}
+            >
+              <FontAwesomeIcon icon={faSearch} className="icon" />{" "}
+              <span>Buscar</span>
+            </button>
+          </li>
+          <li>
+            <button
+              className={`sidebar-btn ${location.pathname === '/es/home/completadas' ? 'active' : ''}`}
+              onClick={() => {
+                setShowSidebar(!showSidebar);
+                navigate("/es/home/completadas");
+              }}
+            >
+              <FontAwesomeIcon icon={faCheck} className="icon" />{" "}
+              <span>Completadas</span>
+            </button>
+          </li>
+          <li>
+            <button
+              className={`sidebar-btn ${location.pathname === '/es/home/profile' ? 'active' : ''}`}
+              onClick={() => {
+                setShowSidebar(!showSidebar);
+                navigate("/es/home/profile");
+              }}
+            >
+              <FontAwesomeIcon icon={faUser} className="icon" />{" "}
+              <span>Perfil</span>
+            </button>
+          </li>
+          <li className="sidebar-logout">
+            <button className="sidebar-btn" onClick={restartLogin}>
+              <FontAwesomeIcon icon={faSignOutAlt} className="icon" />{" "}
+              <span>Cerrar sesion</span>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
