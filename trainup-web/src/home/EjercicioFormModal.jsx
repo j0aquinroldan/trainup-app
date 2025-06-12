0db8908b4545d4665ccb8712e7d8efd4742363ae
Roldan Joaquin <jroldanarin@gmail.com>
2025-06-12 12:55:18 -0300

    refactor: modal de ejercicio en componente

---
+++ trainup-web/src/home/EjercicioFormModal.jsx
@@ -0,0 +1,115 @@
+import { Form, Input, InputNumber, Modal } from "antd";
+import React, { useEffect, useState } from "react";
+
+const EjercicioFormModal = ({
+  open,
+  onCancel,
+  onSubmit,
+  confirmLoading,
+  ejercicio,
+  editedFields,
+  handleFieldChange,
+  form,
+  isFormInvalid,
+}) => {
+
+  useEffect(() => {
+    if (ejercicio) {
+      form.setFieldsValue(editedFields);
+    } else {
+      form.resetFields();
+    }
+  }, [ejercicio, form, editedFields]);
+
+  return (
+    <Modal
+      title={
+        ejercicio
+          ? `Editar ejercicio: ${ejercicio.nombre}`
+          : "Crear Nuevo Ejercicio"
+      }
+      open={open}
+      onCancel={onCancel}
+      onOk={onSubmit}
+      confirmLoading={confirmLoading}
+      okButtonProps={{ disabled: isFormInvalid() }}
+      className="custom-modal"
+      modalRender={(modal) => (
+        <div onClick={(e) => e.stopPropagation()}>{modal}</div>
+      )}
+    >
+      <Form
+        layout="vertical"
+        initialValues={editedFields}
+        onValuesChange={(changedValues) => handleFieldChange(changedValues)}
+        form={form}
+      >
+        <Form.Item
+          label="Nombre"
+          name="nombre"
+          rules={[
+            { required: true, message: "El nombre no puede estar vacío." },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Descripción"
+          name="descripcion"
+          rules={[
+            {
+              required: true,
+              message: "La descripción no puede estar vacía.",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Repeticiones"
+          name="repeticiones"
+        >
+          <InputNumber min={1} />
+        </Form.Item>
+        <Form.Item
+          label="Peso"
+          name="peso"
+        >
+          <InputNumber min={0}/>
+        </Form.Item>
+        <Form.Item
+          label="Músculo"
+          name="musculo"
+          rules={[
+            { required: true, message: "El músculo no puede estar vacío." },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Equipo"
+          name="equipo"
+          rules={[
+            { required: true, message: "El equipo no puede estar vacío." },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          label="Instrucciones"
+          name="instrucciones"
+          rules={[
+            {
+              required: true,
+              message: "Las instrucciones no pueden estar vacías.",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+export default EjercicioFormModal;
--- trainup-web/src/home/Rutina.jsx
+++ trainup-web/src/home/Rutina.jsx
@@ -1,111 +1,179 @@
-import React, { useState, useEffect } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { useLogin } from '../context/LoginContext';
-import FollowBtn from './FollowBtn';
-import Loader from '../utils/Loader';
-import Ejercicio from './Ejercicio';
-import { Button } from 'antd';
-import { obtenerRutinaPorId } from '../api/Api';
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useLogin } from "../context/LoginContext";
+import FollowBtn from "./FollowBtn";
+import Loader from "../utils/Loader";
+import Ejercicio from "./Ejercicio";
+import { Button, Form } from "antd";
+import { obtenerRutinaPorId } from "../api/Api";
+import EjercicioFormModal from "./EjercicioFormModal";
 
 const Rutina = () => {
-    const location = useLocation();
-    const navigate = useNavigate();
-    const { rutinaID, nombre } = location.state || {};
-    const { user } = useLogin();
-    const [isFollowed, setIsFollowed] = useState(false);
-    const [isLoading, setIsLoading] = useState(true);
-    const [listaDeEjercicios, setListaDeEjercicios] = useState([])
-    const [dificultad, setDificultad] = useState('')
-    const [descripcion, setDescripcion] = useState('')
-    const [imgSrc, setImgSrc] = useState('')
-    const [categoria, setCategoria] = useState('')
-    
-    useEffect(() => {
-        obtenerRutinaPorId(rutinaID).then(({ data }) => {
-            setListaDeEjercicios(data.ejercicios)
-            setDescripcion(data.descripcion)
-            setDificultad(data.dificultad)
-            setImgSrc(data.img)
-            setCategoria(data.categoria)
-        }).catch((error) => {
-            console.log(error)
-        })
+  const location = useLocation();
+  const { rutinaID, nombre } = location.state || {};
+  const { user } = useLogin();
+  const [form] = Form.useForm();
+  const [ isOpen, setIsOpen ] = useState(false);
+  const [isFollowed, setIsFollowed] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [listaDeEjercicios, setListaDeEjercicios] = useState([]);
+  const [dificultad, setDificultad] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [imgSrc, setImgSrc] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [editedFields, setEditedFields] = useState({});
+  const [isCreating, setIsCreating] = useState(false);
 
-        if (user) {
-            const sigueRutina = user.rutinasSeguidas.some(rutina => rutina.id === rutinaID);
-            setIsFollowed(sigueRutina);
-            setIsLoading(false);
-        }
+  const handleFieldChange = (changedValues) => {
+    setEditedFields((prevFields) => ({
+      ...prevFields,
+      ...changedValues,
+    }));
+  };
 
-    }, [user, rutinaID]);
+  const isFormInvalid = () => {
+    const requiredFields = [
+      "nombre",
+      "descripcion",
+      "musculo",
+      "equipo",
+      "instrucciones",
+    ];
+    return requiredFields.some((field) => !editedFields[field]);
+  };
 
-
-    if (isLoading) {
-        return (
-            <div className="rutinas-completadas-container">
-                <Loader />
-            </div>
-        );
-    }
-
-    const handleCreateExercise = () => {
-        navigate('/es/home/crear/ejercicio', { state: { rutinaID } }); // Pasar rutinaID al navegar
+  const handleCreateExercise = async () => {
+      try {
+        await form.validateFields();
+        setIsCreating(true);
+        const newExercise = await agregarEjercicioARutina(editedFields);
+        notification.success({
+          message: "¡Éxito!",
+          description: `El ejercicio "${newExercise.nombre}" ha sido creado exitosamente.`,
+          placement: "topRight",
+        });
+        setIsModalVisible(false);
+      } catch (error) {
+        notification.error({
+          message: "Error al crear",
+          description: `No se pudo crear el ejercicio.`,
+          placement: "topRight",
+        });
+      } finally {
+        setIsCreating(false);
+      }
     };
 
-    const deleteEjercicio = (ejercicio) => {
-        const ejercicios = listaDeEjercicios.filter(e => e.id !== ejercicio.id);
-        setListaDeEjercicios(ejercicios);
+  useEffect(() => {
+    obtenerRutinaPorId(rutinaID)
+      .then(({ data }) => {
+        setListaDeEjercicios(data.ejercicios);
+        setDescripcion(data.descripcion);
+        setDificultad(data.dificultad);
+        setImgSrc(data.img);
+        setCategoria(data.categoria);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    if (user) {
+      const sigueRutina = user.rutinasSeguidas.some(
+        (rutina) => rutina.id === rutinaID
+      );
+      setIsFollowed(sigueRutina);
+      setIsLoading(false);
     }
+  }, [user, rutinaID]);
 
-    const updateEjercicio = (ejercicio) => {
-        const ejercicios = listaDeEjercicios.map(e => {
-            if (e.id === ejercicio.id) {
-                return ejercicio;
-            }
-            return e;
-        });
-        setListaDeEjercicios(ejercicios);
-    }
-
-    console.log(imgSrc)
-
+  if (isLoading) {
     return (
-        <div className='rutina-header'>
-            <div className="rutina-container">
-                {/* <div className="rutina-info"> */}
-                    {imgSrc && (
-                    <img src={imgSrc} className="rutina-img" alt={`imagen de rutina ${nombre}`} />
-                )}
-                    <h1 className="rutina-title">{nombre}</h1>
-                    <h2 className='rutina-subtitle' >{descripcion}</h2>
-                    <div className="card-footer">
-                        <p className='category'>{categoria}</p>
-                        <p className={'category ' + `${dificultad.toLowerCase()}`}>{dificultad}</p>
-                    </div>
-
-                    {user.rol ==="ADMIN" ? (
-                        <Button onClick={handleCreateExercise} type="primary">
-                            Crear Ejercicio
-                        </Button>
-                    ) : (
-                        <FollowBtn initFollow={isFollowed} rutinaID={rutinaID} />
-                    )}
-                {/* </div> */}
-
-                
-            </div>
-
-            
-
-            <div className='container-boxinfo overflow-y-auto padding-r-s flex-column'>
-                <h1 className="rutina-title">Ejercicios</h1>
-                {listaDeEjercicios.map(ejercicio => (
-                    <Ejercicio key={ejercicio.id} updateEjercicio={updateEjercicio} deleteEjercicio={deleteEjercicio} ejercicio={ejercicio} rutinaID={rutinaID} />
-                ))}
-            </div>
-        </div>
+      <div className="rutinas-completadas-container">
+        <Loader />
+      </div>
     );
+  }
 
+
+  const deleteEjercicio = (ejercicio) => {
+    const ejercicios = listaDeEjercicios.filter((e) => e.id !== ejercicio.id);
+    setListaDeEjercicios(ejercicios);
+  };
+
+  const updateEjercicio = (ejercicio) => {
+    const ejercicios = listaDeEjercicios.map((e) => {
+      if (e.id === ejercicio.id) {
+        return ejercicio;
+      }
+      return e;
+    });
+    setListaDeEjercicios(ejercicios);
+  };
+
+  console.log(imgSrc);
+
+  return (
+    <div className="rutina-header">
+      <div className="rutina-container">
+        {/* <div className="rutina-info"> */}
+        {imgSrc && (
+          <img
+            src={imgSrc}
+            className="rutina-img"
+            alt={`imagen de rutina ${nombre}`}
+          />
+        )}
+        <h1 className="rutina-title">{nombre}</h1>
+        <h2 className="rutina-subtitle">{descripcion}</h2>
+        <div className="card-footer">
+          <p className="category">{categoria}</p>
+          <p className={"category " + `${dificultad.toLowerCase()}`}>
+            {dificultad}
+          </p>
+        </div>
+
+        {user.rol === "ADMIN" ? (
+          <></>
+        ) : (
+          <FollowBtn initFollow={isFollowed} rutinaID={rutinaID} />
+        )}
+        {/* </div> */}
+      </div>
+
+      <div className="container-boxinfo overflow-y-auto padding-r-s flex-column">
+        <div className="flex gap-m ai-center">
+          <h1 className="rutina-title">Ejercicios</h1>
+          {user.rol === "ADMIN" ? (
+            <button onClick={() => setIsOpen(true)} className="default-btn-3">
+              Agregar ejercicio
+            </button>
+          ) : (
+            <></>
+          )}
+        </div>
+        <EjercicioFormModal
+          open={isOpen}
+          onCancel={() => setIsOpen(false)}
+          onSubmit={handleCreateExercise}
+          confirmLoading={isCreating}
+          ejercicio={null}
+          editedFields={{}}
+          handleFieldChange={handleFieldChange}
+          form={form}
+          isFormInvalid={isFormInvalid}
+        />
+        {listaDeEjercicios.map((ejercicio) => (
+          <Ejercicio
+            key={ejercicio.id}
+            updateEjercicio={updateEjercicio}
+            deleteEjercicio={deleteEjercicio}
+            ejercicio={ejercicio}
+            rutinaID={rutinaID}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Rutina;
